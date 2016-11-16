3327312d7112a31d05df870630f40aa160c21eab
JRABECK <JAKERABECK@GMAIL.COM>
2016-11-15 20:54:52 -0800

    redirects

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,17 +4,44 @@ class ContactsController < ApplicationController
 	@contacts = Contact.all
 	end
 
-	def new_contact
-
-
-	end
-
 	def africa
 	end
 
-	def saved_contact
-			@contact = Contact.new(first_name: params[:first_name], last_name: params[:last_name], phone_number: params[:phone_number], email: params[:email]) 
-	@contact.save
+	def new
 	end
+
+	def create
+				@contact = Contact.new(first_name: params[:first_name], last_name: params[:last_name], phone_number: params[:phone_number], email: params[:email]) 
+	@contact.save
+
+	redirect_to "/contacts/#{contact.id}"
+	end
+
+	def index
+		@contacts = Contact.all 
+	end
+
+	def edit
+
+	end
+
+	def update
+					@contact = Contact.new(first_name: params[:first_name], last_name: params[:last_name], phone_number: params[:phone_number], email: params[:email]) 
+	@contact.save
+
+	redirect_to "/contacts/#{@contact.id}"
+	end
+
+	def show
+		@contact = Contact.find_by(id: params[:id])
+	end
+
+	def destroy
+		@contact = Contact.find_by(id: params[:id])
+		@contact.destroy
+
+		redirect_to "/index"
+	end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,24 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/", to: "contacts#home"
   get "/africa", to: "contacts#africa"
-  get "/new_contact", to: "contacts#new_contact"
-  post "/saved_contact", to: "contacts#saved_contact"
+ 
+  
+  
+
+  #index (show everything)
+  get "/index", to: "contacts#index"
+  #new (form for creating new)
+  get "contacts/new", to: "contacts#new"
+  #create (message saying contact created)
+  post "/contacts", to: "contacts#create"
+  #show (show an individual contact)
+  get "/contacts/:id", to: "contacts#show"
+  #destroy (message saying destroyed)
+  delete "contacts/:id", to: "contacts#destroy"
+  #edit (form for updating)
+  get "contacts/:id/edit", to: "contacts#edit"
+  #update (same as create except saying updated)
+  patch "/contacts/:id", to: "contacts#update"
+
+
 end
